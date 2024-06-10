7c51b668bcbb7b763aead70328c94ad9bc5bee96
Julio Grecco <jagrecco@gmail.com>
2024-06-09 23:07:24 -0300

    Cambios varios en el frontend

--- project/core/static/core/js/app.js
+++ project/core/static/core/js/app.js
@@ -6,10 +6,18 @@ function cambia_lista_card(){
     const card_container=document.getElementById("contenedor_card_libros")
     const lista_container=document.getElementById("contenedor_lista_libros")
 
+    svg_lista=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-columns-reverse" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M0 .5A.5.5 0 0 1 .5 0h2a.5.5 0 0 1 0 1h-2A.5.5 0 0 1 0 .5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10A.5.5 0 0 1 4 .5m-4 2A.5.5 0 0 1 .5 2h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 4h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 6h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2A.5.5 0 0 1 .5 8h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 0 1h-8a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1h-10a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5m-4 2a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5m4 0a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5"/>
+    </svg>`
+    svg_cards=`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-table" viewBox="0 0 16 16">
+  <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm15 2h-4v3h4zm0 4h-4v3h4zm0 4h-4v3h3a1 1 0 0 0 1-1zm-5 3v-3H6v3zm-5 0v-3H1v2a1 1 0 0 0 1 1zm-4-4h4V8H1zm0-4h4V4H1zm5-3v3h4V4zm4 4H6v3h4z"/>
+</svg>`
+    
     if (txt_boton.value=='card'){
 
         txt_boton.value='lista'
-        txt_boton.textContent='Cambiar a Cards'
+        txt_boton.innerHTML=svg_cards
+        
         card_container.className="contenedor_invisible"
         lista_container.className="contenedor_lista_libros"
     } 
@@ -17,10 +25,48 @@ function cambia_lista_card(){
     {
 
         txt_boton.value='card'
-        txt_boton.textContent='Cambiar a Lista'
+        txt_boton.innerHTML=svg_lista
+        
         card_container.className="contenedor_card_libros"
         lista_container.className="contenedor_invisible"
     }
 
 
+}
+
+function expand_card(id){
+    
+    const start_modal=document.getElementById("start_modal")
+    const modal_title=document.getElementById("exampleModalLabel")
+    const modal_body=document.getElementById("modal_body")
+    modal_body.innerHTML=""
+
+    const card=document.getElementById(id)
+
+    const nietos = card.querySelector("div").querySelectorAll(".libro")
+
+    console.log(nietos)
+    modal_title.textContent=nietos[0].textContent
+
+    const contenedor=document.createElement("div")
+
+    contenedor.setAttribute("class", "libro_detail")
+
+    let nuevo_nietos=[]
+
+    nietos.forEach((nieto)=>{
+        nuevo_nietos.push(nieto.cloneNode(true))
+    })
+           
+    nuevo_nietos.forEach((nieto, index)=>{
+        
+        nieto.classList.remove('invisible_field')
+        if (index>0){
+            
+            modal_body.appendChild(contenedor).appendChild(nieto)
+        }
+    })
+    
+    start_modal.click()
+    
 }
